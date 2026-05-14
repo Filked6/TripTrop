84a6664de28ecdc27bf4f7506da9d8479e16c2ab
Filip <filip.kedzior6@gmail.com>
2026-05-14 12:35:52 +0200

    Wzorce do obsługi danych

---
+++ app/src/main/java/pl/filked/triptrop/data/ClosestJourneyData.kt
@@ -0,0 +1,3 @@
+package pl.filked.triptrop.data
+
+data class ClosestJourneyData(val distance: Int, val journeyName: String, val listOfRiddles: List <String>?)
---
+++ app/src/main/java/pl/filked/triptrop/data/JourneyData.kt
@@ -0,0 +1,3 @@
+package pl.filked.triptrop.data
+
+data class JourneyData(val journeysTitle: String ,val journeyName: String, val journeyDetails: String, val journeyCost: Int, val journeyPhoto: Int)
